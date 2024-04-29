5bb6d0fabbbce36a26c8f20761003afb1430259d
takumin0423 <takumin0423baske@gmail.com>
2024-04-29 13:56:36 +0900

    2.1.2

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,8 @@
-use proconio::input;
-
 fn main() {
-    input! {
-        mut n: usize
-    };
+    let a = 25;
+    let b = 4;
+    let c = 12;
 
-    let mut result: String = String::new();
-
-    while n >= 1 {
-        if n % 2 == 1 {
-            result = String::from("1") + &result;
-        } else if n % 2 == 0 {
-            result = String::from("0") + &result;
-        }
-
-        n /= 2;
-    }
-
-    println!("{}", result);
+    println!("{}", a + b + c);
+    println!("{}", a * b * c);
 }
