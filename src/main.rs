94744d62058bfb2163b83715c575dee4d94b7eb1
takumin0423 <takumin0423baske@gmail.com>
2024-05-04 16:34:20 +0900

    2.3.4

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use proconio::input;
 
 fn main() {
-    println!("{}", 8f64.log2());
-    println!("{}", 100f64.powf(1.5));
-    println!("{}", i64::max(20, 21));
-    println!("{}", i64::min(20, 21));
+    println!("{}", func1(20));
+}
+
+fn func1(n: u32) -> i32 {
+    2_i32.pow(n)
 }
