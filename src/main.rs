fb5aee07f77c4a3841189e9ab1ead628af95f4b8
takumin0423 <takumin0423baske@gmail.com>
2024-05-04 12:37:50 +0900

    2.3.1

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,11 @@
 use proconio::input;
 
 fn main() {
-    let cnt = 1000;
-
-    println!("{}", func1());
-    println!("{}", func2(100, cnt));
-    println!("{}", cnt);
+    println!("{}", func1(1.0));
+    println!("{}", func1(5.0));
+    println!("{}", func1(10.0));
 }
 
-fn func1() -> i32 {
-    return 2021;
-}
-
-fn func2(pos: i32, mut cnt: i32) -> i32 {
-    cnt += 1;
-
-    return pos + cnt;
+fn func1(n: f64) -> f64 {
+    n.powi(3)
 }
