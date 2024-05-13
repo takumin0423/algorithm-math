ea50c89add54daf0f820ad3e1818b9030547cbe5
takumin0423 <takumin0423baske@gmail.com>
2024-05-13 22:29:37 +0900

    2.5.3

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,14 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
-        s: usize
+        n: usize
     }
 
-    let mut count = 0;
+    let mut result = 1;
 
-    for i in 1..=n {
-        for j in 1..=n {
-            if i + j <= s {
-                count += 1;
-            }
-        }
+    for i in 2..=n {
+        result *= i;
     }
 
-    println!("{}", count);
+    println!("{}", result);
 }
